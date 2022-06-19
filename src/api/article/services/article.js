bc69fbfe68b981cfe54c3569b2f8ddb1dd024006
ryandewillis <dewillisr@yahoo.com>
2022-06-19 00:34:45 -0500

    Implement custom findOne service for article

--- src/api/article/services/article.js
+++ src/api/article/services/article.js
@@ -6,4 +6,10 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::article.article');
+module.exports = createCoreService('api::article.article', ({ strapi }) => ({
+  async findOne(...args) {
+    const article = await super.findOne(args);
+    console.log(article);
+    return article;
+  }
+}));
