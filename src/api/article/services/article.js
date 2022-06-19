71623bea895eabc9fe17ed305ba5f3dc5ec2be41
ryandewillis <dewillisr@yahoo.com>
2022-06-19 00:58:55 -0500

    Unfurl embedded media before sending response

--- src/api/article/services/article.js
+++ src/api/article/services/article.js
@@ -5,11 +5,31 @@
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
+const { extract } = require("oembed-parser");
+
+const oembedRegex = /\<oembed url\=\"(.*)\"\>\<\/oembed\>/;
+
+const getOembed = async (url) => {
+  try {
+    return await extract(url);
+  } catch (err) {
+    return null;
+  }
+}
 
 module.exports = createCoreService('api::article.article', ({ strapi }) => ({
   async findOne(...args) {
     const article = await super.findOne(args);
-    console.log(article);
+
+    if (oembedRegex.test(article.body)) {
+      const [ oembedNode, oembedUrl ] = article.body.match(oembedRegex);
+      const oembedData = await getOembed(oembedUrl);
+
+      if (oembedData) {
+        article.body = article.body.replace(oembedNode, oembedData.html);
+      }
+    }
+
     return article;
   }
 }));
