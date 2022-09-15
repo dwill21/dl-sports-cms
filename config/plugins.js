3a937b0358a8136b380193086ef791599267413a
ryandewillis <dewillisr@yahoo.com>
2022-09-15 15:08:11 -0500

    Add AWS secrets to provider config for use in nonprod

--- config/plugins.js
+++ config/plugins.js
@@ -3,6 +3,8 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
         region: env('AWS_REGION'),
         params: {
           Bucket: env('AWS_BUCKET'),
